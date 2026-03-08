998cf0785c9b0db1208213daf178d6a4bffa7a73
Radhe <sahuradhekrishna910@gmail.com>
2026-03-08 12:48:16 +0530

    fix layout and config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-
-  // ✅ Fix: moved out of experimental in Next.js 14.2+
-  serverExternalPackages: ['@prisma/client', 'bcryptjs', 'ioredis'],
-
-  images: {
-    remotePatterns: [{ protocol: 'https', hostname: '**' }],
-    dangerouslyAllowSVG: true,
-  },
-
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), 'bcryptjs', '@prisma/client']
-    }
-    return config
-  },
-}
-
+const nextConfig = {}
 module.exports = nextConfig
